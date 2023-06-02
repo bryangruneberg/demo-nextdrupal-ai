739ff246ce0f3d8a206d269c95c7494ca90bd40e
Justin Levi Winter <justinlevi@gmail.com>
2023-06-02 17:33:12 -0400

    working

--- pages/api/util.ts
+++ pages/api/util.ts
@@ -11,12 +11,12 @@ Follow Up Input: {question}
 Standalone question:`);
 
 const QA_PROMPT = PromptTemplate.fromTemplate(
-  `You are an AI assistant for the open source library LangChain. The documentation is located at https://langchain.readthedocs.io.
-You are given the following extracted parts of a long document and a question. Provide a conversational answer with a hyperlink to the documentation.
+  `You are an AI assistant for the cooking and recipe website https://drupal.demo.composetheweb.com. The recipes and articles are located at https://drupal.demo.composetheweb.com.
+You are given the following extracted parts of a long document and a question. Provide a conversational answer with a hyperlink to the recipe or article.
 You should only use hyperlinks that are explicitly listed as a source in the context. Do NOT make up a hyperlink that is not listed.
-If the question includes a request for code, provide a code block directly from the documentation.
+If the question includes a request for a recipe, provide the full recipe with a list of ingredients.
 If you don't know the answer, just say "Hmm, I'm not sure." Don't try to make up an answer.
-If the question is not about LangChain, politely inform them that you are tuned to only answer questions about LangChain.
+If the question is not about a recipe or ingredients or composetheweb, politely inform them that you are tuned to only answer questions about food, recipes, and composetheweb.
 Question: {question}
 =========
 {context}
